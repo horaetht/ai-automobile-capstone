f19f9f8964393908fbaa9ce9a1c3b1c45ab5b399
horaetht <horace32162@gmail.com>
2026-07-16 20:29:46 -0700

    Initialize React frontend with Vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="page">
+      <h1>Online Garage</h1>
+      <p>React Migration in Progress</p>
+    </main>
+  )
+}
+
+export default App
